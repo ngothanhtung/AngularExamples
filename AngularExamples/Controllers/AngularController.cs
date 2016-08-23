801e98ff5ef62cd0fed361151132bd6d1d5cc9eb
TUNGNT <TUNGNT@WINDOWS10>
2016-08-23 19:13:06 +0700

    Add project files.

---
+++ AngularExamples/Controllers/AngularController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AngularExamples.Controllers
+{
+    public class AngularController : Controller
+    {
+        // GET: Angular
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ AngularExamples/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AngularExamples.MvcApplication" Language="C#" %>
